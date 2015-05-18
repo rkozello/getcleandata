32192ca9ff052f989bd03e48e228cf7058d25eb1
rkozello <roman.kozello@gmail.com>
2015-05-18 14:36:56 +0300

    Up to step 4

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,33 @@
-# This is an R script
-# test and train folders from 'UCI HAR Dataset' 
-# must be moved to R working directory
-# Create list of filenames to import
-list_test <- file.path("test", "Inertial Signals", list.files("test/Inertial Signals"))
-list_train <- file.path("train", "Inertial Signals", list.files("train/Inertial Signals"))
-# Read files to fist of dataframes, one for test, one for train
-test_all_data <- lapply(list_of_test, read.table)
-train_all_data <- lapply(list_of_train, read.table)
+# Load data.table library first
+library(data.table)
+# 'UCI HAR Dataset' must reside in the working directory, unzipped
+# Filenames of data to import
+X_test <- "UCI HAR Dataset/test/X_test.txt"
+X_train <- "UCI HAR Dataset/train/X_train.txt"
+# Create descriptive activities columns for test and train
+activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", sep=" ", col.names=c("V1", "Activity"))
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+test_act <- merge(y_test, activity_names, sort=F)[2]
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+train_act <- merge(y_train, activity_names, sort=F)[2]
+# Read subject numbers from subject_[test|train].txt
+subj_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names="Subject")
+subj_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names="Subject")
+# Read column names from features.txt
+features <- read.table("UCI HAR Dataset/features.txt", sep=" ")
+# Create vector for column names
+columns <- features$V2
+rm(features)
+# Read files to dataframes
+X_test_data <- read.table(X_test, col.names=columns)
+X_test_mean_std <-X_test_data[, c(grep("(mean|std)", names(X_test_data)))]
+X_train_data <- read.table(X_train, col.names=columns)
+X_train_mean_std <-X_train_data[, c(grep("(mean|std)", names(X_train_data)))]
+rm(X_test_data, X_train_data)
+# Add activities and subject to data
+X_test_mean_std <- cbind(X_test_mean_std, test_act, subj_test)
+X_train_mean_std <- cbind(X_train_mean_std, train_act, subj_train)
+rm(test_act, train_act)
+# Merge train and test to one dataset
+X_mean_std <- rbind(X_test_mean_std, X_train_mean_std)
+rm(X_test_mean_std, X_train_mean_std)
