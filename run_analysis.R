150fe9f80dfc8dca67acf117696b1e67d6a05e54
rkozello <roman.kozello@gmail.com>
2015-05-18 10:35:31 +0300

    First edition

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,9 @@
 # This is an R script
+# test and train folders from 'UCI HAR Dataset' 
+# must be moved to R working directory
+# Create list of filenames to import
+list_test <- file.path("test", "Inertial Signals", list.files("test/Inertial Signals"))
+list_train <- file.path("train", "Inertial Signals", list.files("train/Inertial Signals"))
+# Read files to fist of dataframes, one for test, one for train
+test_all_data <- lapply(list_of_test, read.table)
+train_all_data <- lapply(list_of_train, read.table)
