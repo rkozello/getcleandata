86abaf76e1c8a47c802a9e7371e44cca9cf4a7f8
rkozello <roman.kozello@gmail.com>
2015-06-12 11:58:14 +0300

    New version

--- run_analysis.R
+++ run_analysis.R
@@ -3,39 +3,39 @@ library(data.table)
 library(plyr)
 # 'UCI HAR Dataset' must reside in the working directory, unzipped
 
-# Create descriptive activities columns for test and train
+# Create descriptive activities dataframe
 activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", sep=" ", col.names=c("V1", "Activity"))
+# Create dataframe of activity codes (y)
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-test_act <- merge(y_test, activity_names, sort=F)[2]
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-train_act <- merge(y_train, activity_names, sort=F)[2]
+y<-rbind(y_test, y_train)
 
-# Read subject numbers from subject_[test|train].txt
-test_subj <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names="Subject")
-train_subj <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names="Subject")
+# Create dataframe of subjects (subj)
+test_subj <-read.table("UCI HAR Dataset/test/subject_test.txt", col.names="Subject", stringsAsFactors=F)
+train_subj <-read.table("UCI HAR Dataset/train/subject_train.txt", col.names="Subject", stringsAsFactors=F)
+subj <- rbind(test_subj, train_subj)
 
-# Read column names from features.txt
+# Read column names from features.txt (columns)
 columns <- (read.table("UCI HAR Dataset/features.txt", sep=" "))$V2
 
-# Read files to dataframe and leave only columns containing '.mean.' and '.std.'
-# but not meanFreq
+# Read test and train files to single dataframe (X_data) 
+# and leave only columns containing '.mean.' and '.std.' (X_mean_std)
 X_test_data <- read.table("UCI HAR Dataset/test/X_test.txt", col.names=columns)
-X_test_mean_std <-X_test_data[, c(grep("(\\.mean\\.|\\.std\\.)", names(X_test_data)))]
 X_train_data <- read.table("UCI HAR Dataset/train/X_train.txt", col.names=columns)
-X_train_mean_std <- X_train_data[, c(grep("(\\.mean\\.|\\.std\\.)", names(X_train_data)))]
-rm(X_test_data, X_train_data)
+X_data <- rbind(X_test_data, X_train_data)
+X_mean_std <-X_data[, c(grep("(\\.mean\\.|\\.std\\.)", names(X_data)))]
 
-# Add activities and subject to data
-X_test_mean_std <- cbind(X_test_mean_std, test_act, test_subj)
-X_train_mean_std <- cbind(X_train_mean_std, train_act, train_subj)
-rm(test_act, train_act, test_subj, train_subj)
+# Add activities and subject codes to data
+X_mean_std <- cbind(y, subj, X_mean_std)
+# Add activity names to data
+X_mean_std <- merge(activity_names, X_mean_std, sort=F)
+# Remove activity codes from resulting data frame
+X_mean_std$V1 <- NULL
 
-# Merge train and test to one dataset
-X_mean_std <- rbind(X_test_mean_std, X_train_mean_std)
-rm(X_test_mean_std, X_train_mean_std)
+# Create a function to round results (make them more readable) 
+roundmean <- function(x) round(mean(x), 4)
+# Calculate mean of each variable for every activity and subject code
+X_average <- ddply(X_mean_std, .(Activity, Subject), numcolwise(roundmean))
 
-# Calculate mean for each variable for every Activity and Subject
-X_average <- ddply(X_mean_std, .(Subject, Activity), numcolwise(mean, na.rm=T))
-rm(X_mean_std)
 # Write resulting data frame to txt file using write.table()
 write.table(X_average, file="course_project_result.txt", row.name=F)
